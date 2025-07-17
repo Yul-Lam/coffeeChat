e2a58be2cb1c1f0b0e2d6e02cd1d7ca57f38f0cc
Yul-Lam <gatkuothyullam@gmail.com>
2025-07-17 13:07:52 +0300

    Update chat.blade.php

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -149,7 +149,7 @@
 
     Echo.private(`chat.{{ $loggedID }}`)
       .listenForWhisper('typing', e => {
-        document.getElementById('typing-indicator').innerText = `${e.userName} typing...`;
+        document.getElementById('typing-indicator').innerText = `${e.userName} is typing...`;
       })
       .listenForWhisper('stopped-typing', () => {
         document.getElementById('typing-indicator').innerText = '';
